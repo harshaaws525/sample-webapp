1ee592c699eaf88c6ee5ea4fb3a1e6d306303b0e
Harsha5905 <harshavardhan5905@gmail.com>
2026-04-06 09:43:34 +0530

    initial commit

---
+++ src/main/java/com/example/demo/Demoapplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class Demoapplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demoapplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from AWS CodePipeline 🚀";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is running ✅";
+    }
+}
